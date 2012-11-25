7c47219b61673fd110715ca863fb9716a316a086
arata <contact@materializing.net>
2012-11-25 10:12:51 +0900

    プラグイン一覧表示からスラッグ一覧へのアクセス用URLを追加

--- config/config.php
+++ config/config.php
@@ -13,4 +13,5 @@ $title = 'slugプラグイン';
 $description = 'ブログ記事のURLにスラッグを設定できます。';
 $author = 'arata';
 $url = 'http://www.materializing.net/';
+$adminLink = array('plugin' => 'slug', 'controller' => 'slugs', 'action' => 'index');
 $installMessage = 'ブログ記事の投稿画面にスラッグ設定欄が追加されます。';
