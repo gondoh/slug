e2d8c54155c6de0a7a4f9f4d9c4690aae80e3dd1
arata <contact@materializing.net>
2012-12-05 23:01:19 +0900

    スラッグ用のヘルパーを追加

--- views/elements/admin/slug_form.php
+++ views/elements/admin/slug_form.php
@@ -13,8 +13,11 @@
 <br />
 <?php echo $bcForm->hidden('Slug.id') ?>
 <?php echo $bcForm->label('Slug.name', 'スラッグ') ?>
-<?php echo $bcForm->input('Slug.name', array('type' => 'text', 'size' => 40, 'maxlength' => 255, 'counter' => true)) ?>
-<?php echo $bcForm->error('Slug.name') ?>
+<?php if($slug->jedgeAppearInputSlug($this->data['SlugConfig']['permalink_structure'])): ?>
+	<?php echo $bcForm->input('Slug.name', array('type' => 'text', 'size' => 40, 'maxlength' => 255, 'counter' => true)) ?>
+<?php else: ?>
+	<?php echo $bcForm->hidden('Slug.name') ?>
+<?php endif ?>
 
 <?php echo $bcForm->input('Slug.status', array('type' => 'checkbox', 'label' => '有効')) ?>
 <?php echo $bcForm->error('Slug.status') ?>
@@ -24,23 +27,24 @@
 	<ul>
 		<li>ブログ記事URLを任意の文字列として表示します。</li>
 		<li>「有効」のチェックボックスにチェックを入れることで、この記事に対してのスラッグが有効化されます。</li>
-		<li>表示形式は <small>http://〜/BLOG/archives/設定文字列</small> となります。</li>
+		<li>表示形式は <small>http://〜/BLOG/archives/設定スラッグ</small> となります。</li>
 	</ul>
 </div>
+<?php echo $bcForm->error('Slug.name') ?>
 
 <?php if($this->action == 'admin_edit'): ?>
 <div class="box-tolink align-left">
 	<?php if($bcForm->value('BlogPost.status')): ?>
 		<?php if($bcForm->value('Slug.name') && $bcForm->value('Slug.status')): ?>
 	URL：<?php $bcBaser->link(
-			$bcBaser->getUri('/' . $blogContent['BlogContent']['name'] . '/archives/' . $bcForm->value('Slug.name')),
-			'/' . $blogContent['BlogContent']['name'] . '/archives/' . $bcForm->value('Slug.name')) ?>
+			$bcBaser->getUri('/' . $blogContent['BlogContent']['name'] . $slug->getSlugUrl($this->data['Slug'], $this->data['BlogPost'])),
+			'/' . $blogContent['BlogContent']['name'] . $slug->getSlugUrl($this->data['Slug'], $this->data['BlogPost'])) ?>
 		<?php elseif($bcForm->value('Slug.name')): ?>
-	URL：<?php echo $bcBaser->getUri('/' . $blogContent['BlogContent']['name'] . '/archives/' . $bcForm->value('Slug.name')) ?>
+	URL：<?php echo $bcBaser->getUri('/' . $blogContent['BlogContent']['name'] . $slug->getSlugUrl($this->data['Slug'], $this->data['BlogPost'])) ?>
 		<?php endif ?>
 	<?php else: ?>
 		<?php if($bcForm->value('Slug.name')): ?>
-	URL：<?php echo $bcBaser->getUri('/' . $blogContent['BlogContent']['name'] . '/archives/' . $bcForm->value('Slug.name')) ?>
+	URL：<?php echo $bcBaser->getUri('/' . $blogContent['BlogContent']['name'] . $slug->getSlugUrl($this->data['Slug'], $this->data['BlogPost'])) ?>
 		<?php endif ?>
 	<?php endif ?>
 </div>
---
+++ views/helpers/slug.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * [Helper] slug
+ *
+ * @copyright		Copyright 2012, materializing.
+ * @link			http://www.materializing.net/
+ * @author			arata
+ * @package			slug.views
+ * @version			1.1.0
+ * @license			MIT
+ */
+class SlugHelper extends AppHelper {
+
+/**
+ * スラッグ用URLを生成する
+ * 
+ * @param array $data
+ * @return string
+ * @access public
+ */
+	function getSlugUrl($slug, $data){
+
+		$SlugConfigModel = ClassRegistry::init('Slug.SlugConfig');
+		$SlugConfigs = $SlugConfigModel->findExpanded();
+
+		if($SlugConfigs['permalink_structure'] === '1') {
+			// 記事タイトル
+			return '/archives/' . $data['name'];
+
+		} elseif($SlugConfigs['permalink_structure'] === '2') {
+			// 記事ID
+			return '/archives/' . $data['id'];
+
+		} elseif($SlugConfigs['permalink_structure'] === '3') {
+			// 記事ID（6桁）
+			return '/archives/' . sprintf('%06d', $data['id']);
+
+		} elseif($SlugConfigs['permalink_structure'] === '4') {
+			// /2012/12/01/sample-post/
+			return '/archives/' . date('Y/m/d', strtotime($data['posts_date'])) . '/' . $data['name'];
+
+		} elseif($SlugConfigs['permalink_structure'] === '5') {
+			// /2012/12/sample-post/
+			return '/archives/' . date('Y/m', strtotime($data['posts_date'])) . '/' . $data['name'];
+
+		} else {
+			return '/archives/' . $slug['name'];
+
+		}
+
+	}
+/**
+ * スラッグ入力欄の表示判定を行う
+ * 
+ * @param string $data
+ * @return boolean
+ */
+	function jedgeAppearInputSlug($data) {
+
+		if(!$data) {
+			return true;
+		} elseif($data === '1' || $data === '4' || $data === '5') {
+			// 記事タイトル or /2012/12/01/sample-post/ or /2012/12/sample-post/
+			return true;
+		}
+
+		return false;
+
+	}
+
+}
