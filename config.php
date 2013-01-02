96fd0cfa932b42e1a93b0eefe605431a88765984
arata <contact@materializing.net>
2013-01-03 01:22:38 +0900

    change configファイルの場所を変更

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
  * @copyright		Copyright 2012, materializing.
  * @link			http://www.materializing.net/
  * @author			arata
- * @package			slug.config
+ * @package			slug
  * @license			MIT
  */
 $title = 'slugプラグイン';
@@ -13,4 +13,4 @@ $description = 'ブログ記事のURLにスラッグを設定できます。';
 $author = 'arata';
 $url = 'http://www.materializing.net/';
 $adminLink = array('plugin' => 'slug', 'controller' => 'slug_configs', 'action' => 'index');
-$installMessage = 'ブログ記事の投稿画面にスラッグ設定欄が追加されます。また、スラッグ一括設定より作成しているブログ記事のスラッグをまとめて登録できます。';
+$installMessage = 'ブログ記事の投稿画面にスラッグ設定欄が追加されます。<br />また、スラッグ一括設定より作成しているブログ記事のスラッグをまとめて登録できます。';
