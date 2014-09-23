05e9dd0d8c5958e52a25831c8f9e7d074dcc2b74
arata <contact@materializing.net>
2014-09-23 16:09:00 +0900

    change 3系対応。フック処理をイベント処理に変更

--- Event/SlugControllerEventListener.php
+++ Event/SlugControllerEventListener.php
@@ -9,57 +9,60 @@
  */
 class SlugControllerEventListener extends BcControllerEventListener {
 /**
- * 登録フック
+ * 登録イベント
  *
  * @var array
- * @access public
  */
-	var $registerHooks = array(
-		'initialize', 'startup', 'beforeRender', 'afterBlogPostAdd', 'afterBlogPostEdit', 'beforeRedirect', 'shutdown');
+	public $events = array(
+		'initialize',
+		'startup',
+		'beforeRender',
+		'shutdown'
+	);
+	
 /**
  * コントローラー
  *
  * @var Controller
- * @access public
  */
-	var $controller = null;
+	public $controller = null;
+	
 /**
- * slugヘルパー
+ * Slugヘルパー
  * 
  * @var SlugHelper
- * @access public
  */
-	var $Slug = null;
+	public $Slug = null;
+	
 /**
- * slug設定情報
+ * Slug設定情報
  * 
  * @var array
- * @access public
  */
-	var $slugConfigs = array();
+	public $slugConfigs = array();
+	
 /**
- * slug情報
+ * Slug情報
  * 
  * @var Object
- * @access public
  */
-	var $SlugModel = null;
+	public $SlugModel = null;
+	
 /**
- * slug設定情報
+ * Slug設定情報
  * 
  * @var Object
- * @access public
  */
-	var $SlugConfigModel = null;
+	public $SlugConfigModel = null;
+	
 /**
  * constructer
  * 
  * @return void
- * @access private
  */
-	function __construct() {
+	private function __construct() {
 		parent::__construct();
-
+		
 		if (ClassRegistry::isKeySet('Slug.SlugConfig')) {
 			$this->SlugConfigModel = ClassRegistry::getObject('Slug.SlugConfig');
 		} else {
@@ -67,330 +70,171 @@ class SlugControllerEventListener extends BcControllerEventListener {
 		}
 		$this->slugConfigs = $this->SlugConfigModel->read();
 		$this->SlugModel = ClassRegistry::init('Slug.Slug');
-
+		
 		App::import('Helper', 'Slug.Slug');
 		$this->Slug = new SlugHelper();
-
 	}
+	
 /**
  * initialize
  * 
- * @param Controller $controller 
+ * @param CakeEvent $event
  */
-	function initialize($controller) {
+	public function initialize(CakeEvent $event) {
+		$Controller = $event->subject();
 		// BlogHelper の不在エラーが出るため読込
-		$controller->helpers[] = 'Blog.Blog';
+		$Controller->helpers[] = 'Blog.Blog';
 		// Slugヘルパーの追加
-		$controller->helpers[] = 'Slug.Slug';		
+		$Controller->helpers[] = 'Slug.Slug';		
 	}
+	
 /**
  * startup
  * 
- * @param Controller $controller 
+ * @param CakeEvent $event
  * @return void
- * @access public
  */
-	function startup($controller) {
-
+	public function startup(CakeEvent $event) {
+		$Controller = $event->subject();
 		// ブログ記事へのリンクをクリックした際に実行
 		// ブログ記事ページ表示の際に、記事NOをスラッグに置き換える
-		if(!empty($controller->params['plugin'])) {
-			if($controller->params['plugin'] == 'blog') {
+		if (!empty($Controller->request->params['plugin'])) {
+			if ($Controller->request->params['plugin'] == 'blog') {
 
 				// TODO prefix付だとエラーになるから書き換える
-				
-				if($controller->action == 'archives') {
+				if ($Controller->request->params['action'] == 'archives') {
 
-					// $slug = urldecode($controller->params['pass']['0']);
-					foreach ($controller->params['pass'] as $key => $param) {
-						$controller->params['pass'][$key] = urldecode($param);
+					// $slug = urldecode($Controller->request->params['pass']['0']);
+					foreach ($Controller->request->params['pass'] as $key => $param) {
+						$Controller->request->params['pass'][$key] = urldecode($param);
 					}
 
 					$slug = '';
-					$paramsCount = count($controller->params['pass']);
-					if($paramsCount == 4) {
-						$postDayBegin = $controller->params['pass']['0'] . '/' . $controller->params['pass']['1'] . '/' . $controller->params['pass']['2'];
-						$postDayEnd = $controller->params['pass']['0'] . '/' . $controller->params['pass']['1'] . '/' . $controller->params['pass']['2'] . ' 23:59:59';
-						$slug = $controller->params['pass']['3'];
-					} elseif($paramsCount == 3) {
-						$postDayBegin = $controller->params['pass']['0'] . '/' . $controller->params['pass']['1'] . '/' . '01';
-						$postDayEnd = date($controller->params['pass']['0'] . '-' . $controller->params['pass']['1'] . '-t') . ' 23:59:59';
-						$slug = $controller->params['pass']['2'];
-					} elseif($paramsCount == 2) {
+					$paramsCount = count($Controller->request->params['pass']);
+					if ($paramsCount == 4) {
+						$postDayBegin = $Controller->request->params['pass']['0'] . '/' . $Controller->request->params['pass']['1'] . '/' . $Controller->request->params['pass']['2'];
+						$postDayEnd = $Controller->request->params['pass']['0'] . '/' . $Controller->request->params['pass']['1'] . '/' . $Controller->request->params['pass']['2'] . ' 23:59:59';
+						$slug = $Controller->request->params['pass']['3'];
+					} elseif ($paramsCount == 3) {
+						$postDayBegin = $Controller->request->params['pass']['0'] . '/' . $Controller->request->params['pass']['1'] . '/' . '01';
+						$postDayEnd = date($Controller->request->params['pass']['0'] . '-' . $Controller->request->params['pass']['1'] . '-t') . ' 23:59:59';
+						$slug = $Controller->request->params['pass']['2'];
+					} elseif ($paramsCount == 2) {
 						// TODO カテゴリ名を含む場合のブログ記事詳細
-						$slug = $controller->params['pass']['1'];
+						$slug = $Controller->request->params['pass']['1'];
 					} else {
-						$slug = $controller->params['pass']['0'];
+						$slug = $Controller->request->params['pass']['0'];
 					}
-
-					if($this->slugConfigs['SlugConfig']['permalink_structure'] === '2' || $this->slugConfigs['SlugConfig']['permalink_structure'] === '3') {
+					
+					if ($this->slugConfigs['SlugConfig']['permalink_structure'] === '2' || $this->slugConfigs['SlugConfig']['permalink_structure'] === '3') {
 						// 記事ID or 記事ID（6桁）
 						$conditions = array(
 							'Slug.blog_post_id' => intval($slug),
-							'Slug.blog_content_id' => $controller->blogContent['BlogContent']['id'],
+							'Slug.blog_content_id' => $Controller->blogContent['BlogContent']['id'],
 						);
 					} else {
 						// スラッグ、/2012/12/01/sample-post/、/2012/12/sample-post/
 						$conditions = array(
 							'Slug.name' => $slug,
-							'Slug.blog_content_id' => $controller->blogContent['BlogContent']['id'],
+							'Slug.blog_content_id' => $Controller->blogContent['BlogContent']['id'],
 						);
 					}
-
-					if(!empty($conditions)) {
+					
+					if (!empty($conditions)) {
 						$data = $this->SlugModel->find('first', array('conditions' => $conditions));
 						// ブログ記事NOをURLの引数と見立てている
-						if($data) {
-							$controller->params['pass'][0] = $data['Slug']['blog_post_no'];
+						if ($data) {
+							$Controller->request->params['pass'][0] = $data['Slug']['blog_post_no'];
 						}
 					}
-
+					
 				}
-
+				
 			}
 		}
-
+		
 	}
+	
 /**
  * beforeRender
  * 
- * @param Controller $controller 
+ * @param CakeEvent $event
  * @return void
- * @access public
  */
-	function beforeRender($controller) {
-
-		if($controller->name == 'BlogPosts') {
-
+	public function beforeRender(CakeEvent $event) {
+		$Controller = $event->subject();
+		if ($Controller->name == 'BlogPosts') {
 			// ブログ記事編集・追加画面で実行
-			// TODO startup で処理したかったが、$controller->data に入れるとそれを全て上書きしてしまうのでダメだった
-			if($controller->action == 'admin_edit' || $controller->action == 'admin_add') {
-				
+			// TODO startup で処理したかったが、$Controller->data に入れるとそれを全て上書きしてしまうのでダメだった
+			if ($Controller->request->params['action'] == 'admin_edit' || $Controller->request->params['action'] == 'admin_add') {
 				// スラッグ設定データを記事編集画面に追加
 				$this->slugConfigs = $this->SlugConfigModel->find('first',
 					array(
-						'conditions' => array('SlugConfig.blog_content_id' => $controller->BlogContent->id))
+						'conditions' => array('SlugConfig.blog_content_id' => $Controller->BlogContent->id))
 				);
-				$controller->data['SlugConfig'] = $this->slugConfigs['SlugConfig'];
-
+				$Controller->data['SlugConfig'] = $this->slugConfigs['SlugConfig'];
 			}
-
-			// Ajaxコピー処理時に実行
-			//   ・Ajax削除時は、内部的に Model->delete が呼ばれているため afterDelete で処理可能
-			if($controller->action == 'admin_ajax_copy') {
-				// ブログ記事コピー保存時にエラーがなければ保存処理を実行
-				if(empty($controller->BlogPost->validationErrors)) {
-					$slugDate = array();
-					$slugDate['Slug']['blog_content_id'] = $controller->viewVars['data']['BlogPost']['blog_content_id'];
-					$slugDate['Slug']['blog_post_no'] = $controller->viewVars['data']['BlogPost']['no'];
-					$slugDate['Slug']['name'] = $controller->viewVars['data']['BlogPost']['name'];
-					$slugDate['Slug']['blog_post_id'] = $controller->viewVars['data']['BlogPost']['id'];
-
-					// 重複スラッグを探索して、重複していれば重複個数＋１をつける
-					$duplicateDatas = $this->SlugModel->searchDuplicateSlug($slugDate);
-					if($duplicateDatas) {
-						$slugDate['Slug']['name'] = $this->SlugModel->makeSlugName($duplicateDatas, $slugDate);
-					}
-
-					$this->SlugModel->create($slugDate);
-					$this->SlugModel->save($slugDate, false);
-					// キャッシュの削除を行わないと、登録したスラッグがブログ記事編集画面に反映されない
-					clearAllCache();
-				}
-			}
-
 		}
-
-		if($controller->name == 'BlogContents') {
-
+		
+		if ($Controller->name == 'BlogContents') {
 			// ブログ設定編集画面にスラッグ設定情報を送る
-			if($controller->action == 'admin_edit') {
-				$this->slugConfigs = $this->SlugConfigModel->findByBlogContentId($controller->BlogContent->id);
-				$controller->data['SlugConfig'] = $this->slugConfigs['SlugConfig'];
-				$controller->set('permalink_structure', $this->Slug->addSampleShow($this->SlugConfigModel->permalink_structure));
+			if ($Controller->request->params['action'] == 'admin_edit') {
+				$this->slugConfigs = $this->SlugConfigModel->findByBlogContentId($Controller->BlogContent->id);
+				$Controller->data['SlugConfig'] = $this->slugConfigs['SlugConfig'];
+				$Controller->set('permalink_structure', $this->Slug->addSampleShow($this->SlugConfigModel->permalink_structure));
 			}
 			// ブログ追加画面にスラッグ設定情報を送る
-			if($controller->action == 'admin_add') {
+			if ($Controller->request->params['action'] == 'admin_add') {
 				$defalut = $this->SlugConfigModel->getDefaultValue();
-				$controller->data['SlugConfig'] = $defalut['SlugConfig'];
-				$controller->set('permalink_structure', $this->Slug->addSampleShow($this->SlugConfigModel->permalink_structure));
+				$Controller->data['SlugConfig'] = $defalut['SlugConfig'];
+				$Controller->set('permalink_structure', $this->Slug->addSampleShow($this->SlugConfigModel->permalink_structure));
 			}
-
-			// Ajaxコピー処理時に実行
-			//   ・Ajax削除時は、内部的に Model->delete が呼ばれているため afterDelete で処理可能
-			if($controller->action == 'admin_ajax_copy') {
-				// ブログコピー保存時にエラーがなければ保存処理を実行
-				if(empty($controller->BlogContent->validationErrors)) {
-					$slugConfigDate = $this->SlugConfigModel->findByBlogContentId($controller->params['pass']['0']);
-					// もしスラッグ設定の初期データ作成を行ってない事を考慮して判定している
-					$saveData = array();
-					if($slugConfigDate) {
-						$saveData['SlugConfig']['blog_content_id'] = $controller->viewVars['data']['BlogContent']['id'];
-						$saveData['SlugConfig']['permalink_structure'] = $slugConfigDate['SlugConfig']['permalink_structure'];
-						$saveData['SlugConfig']['ignore_archives'] = $slugConfigDate['SlugConfig']['ignore_archives'];
-					} else {
-						$saveData['SlugConfig']['blog_content_id'] = $controller->viewVars['data']['BlogContent']['id'];
-						$saveData['SlugConfig']['permalink_structure'] = 0;
-						$saveData['SlugConfig']['ignore_archives'] = false;
-					}
-
-					$this->SlugConfigModel->create($saveData);
-					$this->SlugConfigModel->save($saveData, false);
-					// キャッシュの削除を行わないと、登録したスラッグ設定がスラッグ編集画面に反映されない
-					clearAllCache();
-				}
-			}
-
 		}
-
+		
 		// blogPosts、ブログのindex、ブログのarchives で実行
 		// プレビュー時に未定義エラーが出るため判定
-		if(!empty($controller->params['plugin'])) {
-			if($controller->params['plugin'] == 'blog') {
-				if($controller->action == 'posts' || $controller->action == 'index' || $controller->action == 'archives') {
-					foreach ($controller->viewVars['posts'] as $key => $post) {
-						$controller->viewVars['posts'][$key]['BlogPost']['no'] = $this->Slug->getSlugName($post['Slug'], $post['BlogPost']);
+		if (!empty($Controller->params['plugin'])) {
+			if ($Controller->params['plugin'] == 'blog') {
+				if ($Controller->request->params['action'] == 'posts' || $Controller->request->params['action'] == 'index' || $Controller->request->params['action'] == 'archives') {
+					foreach ($Controller->viewVars['posts'] as $key => $post) {
+						$Controller->viewVars['posts'][$key]['BlogPost']['no'] = $this->Slug->getSlugName($post['Slug'], $post['BlogPost']);
 					}
 				}
 				
 				// 公開側ブログ記事詳細表示時、archives除外設定を行っている場合、
 				// /BLOG/archives/detail にアクセスされた場合は notFound にする
 				// TODO この仕様で良いのかどうかはのちのちの意見で再考していく必要あり
-				if ($controller->action == 'archives') {
-					$paramsCount = count($controller->params['pass']);
+				if ($Controller->request->params['action'] == 'archives') {
+					$paramsCount = count($Controller->params['pass']);
 					if ($paramsCount <= 2) {
 						if ($this->slugConfigs['SlugConfig']['ignore_archives']) {
 							$regex = '/\/archives\//';
-							if(preg_match($regex, $controller->params['url']['url'])) {
-								$controller->notFound();
+							if (preg_match($regex, $Controller->params['url']['url'])) {
+								$Controller->notFound();
 							}
 						}
 					}
 				}
 			}
 		}
-
+		
 	}
+	
 /**
  * shutdown
  * 
- * @param Controller $controller 
+ * @param CakeEvent $event
  * @return void
- * @access public
  */
-	function shutdown($controller) {
-
+	public function shutdown(CakeEvent $event) {
+		$Controller = $event->subject();
 		// blogPosts で実行
 		//  ・requestAction で実行と bcBaser->link 未使用のため、出力内容を直接書き換えている
-		if($controller->plugin == 'blog') {
-			if($controller->action == 'posts') {
-				$controller->output = $this->Slug->convertOutputArchivesLink($controller->output);
+		if ($Controller->plugin == 'blog') {
+			if ($Controller->request->params['action'] == 'posts') {
+				$Controller->output = $this->Slug->convertOutputArchivesLink($Controller->output);
 			}
 		}
-
 	}
-/**
- * beforeRedirect
- * 
- * @param Object $controller
- * @param type $url
- * @param type $status
- * @param type $exit
- * @return void
- * @access public
- */
-	function beforeRedirect($controller, $url, $status, $exit) {
-
-		if($controller->name == 'BlogContents') {
-			if($controller->action == 'admin_edit') {
-				// ブログ設定編集保存時に、スラッグ設定情報を保存する
-				$this->SlugConfigModel->set($controller->data['SlugConfig']);
-			} elseif($controller->action == 'admin_add') {
-				// ブログ追加時に、スラッグ設定情報を保存する
-				$controller->data['SlugConfig']['blog_content_id'] = $controller->BlogContent->id;
-				$this->SlugConfigModel->create($controller->data['SlugConfig']);
-			}
-			if(empty($controller->BlogContent->validationErrors)) {
-				if(!$this->SlugConfigModel->save(null, false)) {
-					$this->log(sprintf('ID：%s のスラッグ設定の保存に失敗しました。', $controller->data['SlugConfig']['id']));
-				}
-			}
-		}
-
-	}
-/**
- * afterBlogPostAdd
- *
- * @param Controller $controller
- * @return void
- * @access public
- */
-	function afterBlogPostAdd($controller) {
-
-		// ブログ記事保存時にエラーがなければ保存処理を実行
-		if(empty($controller->BlogPost->validationErrors)) {
-			$this->_slugSaving($controller);
-		}
-
-	}
-/**
- * afterBlogPostEdit
- *
- * @param Controller $controller
- * @return void
- * @access public
- */
-	function afterBlogPostEdit($controller) {
-
-		// ブログ記事保存時にエラーがなければ保存処理を実行
-		if(empty($controller->BlogPost->validationErrors)) {
-			$this->_slugSaving($controller);
-		}
-
-	}
-/**
- * スラッグ情報を保存する
- * 
- * @param Controller $controller 
- * @return void
- * @access private
- */
-	function _slugSaving($controller) {
-
-		$controller->data['Slug']['blog_content_id'] = $controller->data['BlogPost']['blog_content_id'];
-		$controller->data['Slug']['blog_post_no'] = $controller->data['BlogPost']['no'];
-
-		// スラッグが未入力の場合は、ブログ記事タイトルを設定する
-		if(!$controller->data['Slug']['name']) {
-			$controller->data['Slug']['name'] = $controller->data['BlogPost']['name'];
-		}
-
-		if($controller->action == 'admin_add') {
-			$controller->data['Slug']['blog_post_id'] = $controller->BlogPost->getLastInsertId();
-			// 重複スラッグを探索して、重複していれば重複個数＋１をつける
-			$duplicateDatas = $this->SlugModel->searchDuplicateSlug($controller->data);
-			if($duplicateDatas) {
-				$controller->data['Slug']['name'] = $this->SlugModel->makeSlugName($duplicateDatas, $controller->data);
-			}
-		} else {
-			$controller->data['Slug']['blog_post_id'] = $controller->BlogPost->id;
-			// 重複スラッグを探索して、重複していれば重複個数＋１をつける
-			$duplicateDatas = $this->SlugModel->searchDuplicateSlug($controller->data, $controller->data['Slug']['id']);
-			if($duplicateDatas) {
-				$controller->data['Slug']['name'] = $this->SlugModel->makeSlugName($duplicateDatas, $controller->data);
-			}
-		}
-
-		if(empty($controller->data['Slug']['id'])) {
-			$this->SlugModel->create($controller->data['Slug']);
-		} else {
-			$this->SlugModel->set($controller->data['Slug']);
-		}
-
-		if(!$this->SlugModel->save($controller->data['Slug'], false)) {
-			$this->log('ブログ記事ID：' . $controller->data['Slug']['blog_post_id'] . 'のスラッグ情報保存に失敗しました。');
-		}
-
-	}
-
+	
 }
--- Event/SlugHelperEventListener.php
+++ Event/SlugHelperEventListener.php
@@ -9,122 +9,122 @@
  */
 class SlugHelperEventListener extends BcHelperEventListener {
 /**
- * 登録フック
+ * 登録イベント
  *
  * @var array
- * @access public
  */
-	var $registerHooks = array('afterFormInput', 'afterFormCreate', 'afterBaserGetLink', 'afterElement');
+	public $events = array(
+		'Form.afterInput',
+		'Form.afterCreate',
+		'afterBaserGetLink',
+		'afterElement'
+	);
+	
 /**
  * ビュー
  * 
  * @var View 
  */
-	var $View = null;
+	public $View = null;
+	
 /**
  * slugヘルパー
  * 
  * @var SlugHelper
- * @access public
  */
-	var $Slug = null;
+	public $Slug = null;
+	
 /**
  * slug設定情報
  * 
  * @var array
- * @access public
  */
-	var $slugConfigs = array();
+	public $slugConfigs = array();
+	
 /**
  * Construct 
  * 
  */
-	function __construct() {
+	public function __construct() {
 		parent::__construct();
 		$SlugConfigModel = ClassRegistry::init('Slug.SlugConfig');
 		$this->slugConfigs = $SlugConfigModel->read();
 		$this->View = ClassRegistry::getObject('view');
-
+		
 		App::import('Helper', 'Slug.Slug');
 		$this->Slug = new SlugHelper();
 	}
+	
 /**
  * afterFormInput
  * 
- * @param Object $form
- * @param string $fieldName
- * @param string $out
+ * @param CakeEvent $event
  * @return string 
  */
-	function afterFormInput($form, $fieldName, $out) {
-
+	public function formAfterInput(CakeEvent $event) {
+		$Form = $event->subject();
 		// ブログ記事編集画面のタイトル入力欄の下にスラッグ入力欄を表示する
-		if($form->params['controller'] == 'blog_posts'){
-			if($this->action == 'admin_add' || $this->action == 'admin_edit'){
-				if($fieldName == 'BlogPost.name') {
-					$out = $out . $this->View->element('admin/slug_form', array('plugin' => 'slug'));
+		if($Form->request->params['controller'] == 'blog_posts'){
+			if($Form->request->params['action'] == 'admin_add' || $Form->request->params['action'] == 'admin_edit'){
+				if($event->data['fieldName'] == 'BlogPost.name') {
+					$event->data['out'] = $event->data['out'] . $Form->element('Slug.slug_form');
 				}
 			}
 		}
-		return $out;
-
+		return $event->data['out'];
 	}
+	
 /**
  * afterFormCreate
  * 
- * @param Object $form
- * @param string $id
- * @param string $out
+ * @param CakeEvent $event
  * @return string
  */
-	function afterFormCreate($form, $id, $out) {
-
-		if($form->params['controller'] == 'blog_contents'){
+	public function formAfterCreate(CakeEvent $event) {
+		$Form = $event->subject();
+		if ($Form->request->params['controller'] == 'blog_contents'){
 			// ブログ設定編集画面にスラッグ設定欄を表示する
-			if($this->action == 'admin_edit'){
-				if($id == 'BlogContentEditForm') {
-					$out = $out . $this->View->element('admin/slug_config_form', array('plugin' => 'slug'));
+			if ($Form->request->params['action'] == 'admin_edit'){
+				if ($event->data['id'] == 'BlogContentEditForm') {
+					$event->data['out'] = $event->data['out'] . $Form->element('Slug.slug_config_form');
 				}
 			}
 			// ブログ追加画面にスラッグ設定欄を表示する
-			if($this->action == 'admin_add'){
-				if($id == 'BlogContentAddForm') {
-					$out = $out . $this->View->element('admin/slug_config_form', array('plugin' => 'slug'));
+			if ($Form->request->params['action'] == 'admin_add'){
+				if ($event->data['id'] == 'BlogContentAddForm') {
+					$event->data['out'] = $event->data['out'] . $Form->element('Slug.slug_config_form');
 				}
 			}
 		}
-		return $out;
-
+		return $event->data['out'];
 	}
+	
 /**
  * afterBaserGetLink
  * 
- * @param Object $html
+ * @param CakeEvent $event
  * @param string $url
  * @param string $out
  * @return string 
  */
-	function afterBaserGetLink($html, $url, $out) {
-
-		if(empty($this->request->params['prefix']) || ($this->request->params['prefix'] != 'admin')) {
-
+	public function afterBaserGetLink(CakeEvent $event, $url) {
+		$View = $event->subject();
+		if (empty($View->request->params['prefix']) || ($View->request->params['prefix'] != 'admin')) {
 			$parseUrl = Router::parse($url);
 			$PluginContent = ClassRegistry::init('PluginContent');
 			$pluginContent = $PluginContent->currentPluginContent($url);
 			// beforeFind が機能しないために取得し直している
-			if($pluginContent) {
+			if ($pluginContent) {
 				$pluginContent = $PluginContent->find('first', array(
 					'conditions' => array(
 						'PluginContent.name' => $pluginContent['PluginContent']['name'],
 						'PluginContent.plugin' => $pluginContent['PluginContent']['plugin'])));
-				$out = $this->convertOutputArchivesLink($out, $parseUrl, $pluginContent['PluginContent']);
+				$event->data['out'] = $this->convertOutputArchivesLink($event->data['out'], $parseUrl, $pluginContent['PluginContent']);
 			}
-
 		}
-
-		return $out;
-
+		return $event->data['out'];
 	}
+	
 /**
  * archives除外設定が有効な場合は、archives を省いたURLリンクを生成して返す
  * 
@@ -133,24 +133,23 @@ class SlugHelperEventListener extends BcHelperEventListener {
  * @param array $pluginContent
  * @return string 
  */
-	function convertOutputArchivesLink($out = '', $parseUrl = array(), $pluginContent = array()) {
-
+	public function convertOutputArchivesLink($out = '', $parseUrl = array(), $pluginContent = array()) {
 		$countPass = count($parseUrl['pass']);
-
-		if($countPass === 1 || $countPass === 2) {
-			if($countPass === 1) {
+		
+		if ($countPass === 1 || $countPass === 2) {
+			if ($countPass === 1) {
 				$no = $parseUrl['pass']['0'];
-			} elseif($countPass === 2) {
+			} elseif ($countPass === 2) {
 				$no = $parseUrl['pass']['1'];
 			}
-
+			
 			$judgeArchives = false;
-			if($parseUrl['pass']['0'] == 'category' || $parseUrl['pass']['0'] == 'tag' || $parseUrl['pass']['0'] == 'date') {
+			if ($parseUrl['pass']['0'] == 'category' || $parseUrl['pass']['0'] == 'tag' || $parseUrl['pass']['0'] == 'date') {
 				$judgeArchives = true;
 			}
-
-			if(!$judgeArchives) {
-				if($pluginContent) {
+			
+			if (!$judgeArchives) {
+				if ($pluginContent) {
 					$blogContentId = $pluginContent['content_id'];
 				} else {
 					$blogContentId = $this->View->viewVars['blogContent']['BlogContent']['id'];
@@ -164,75 +163,72 @@ class SlugHelperEventListener extends BcHelperEventListener {
 				$data = $BlogPostModel->find('first', array(
 					'conditions' => $conditions,
 					'recursive' => 1));
-				if($data) {
+				if ($data) {
 					$no = $this->Slug->getSlugName($data['Slug'], $data['BlogPost']);
 				}
 				unset($BlogPostModel);
 				unset($data);
 			}
-
 		}
-
+		
 		$pattern = '/href\=\"(.+)\/archives\/(.+)\"/';
-		if(!empty($no)) {
-			if($judgeArchives) {
+		if (!empty($no)) {
+			if ($judgeArchives) {
 				// カテゴリへのリンクの際は category/ を付加する
-				if($parseUrl['pass']['0'] == 'category') {
+				if ($parseUrl['pass']['0'] == 'category') {
 					$no = 'category' . DS . $no;
 				}
 				// タグへのリンクの際は tag/ を付加する
-				if($parseUrl['pass']['0'] == 'tag') {
+				if ($parseUrl['pass']['0'] == 'tag') {
 					$no = 'tag' . DS . $no;
 				}
 				// 年別へのリンクの際は date/ を付加する
-				if($parseUrl['pass']['0'] == 'date') {
+				if ($parseUrl['pass']['0'] == 'date') {
 					$no = 'date' . DS . $no;
 				}
 			}
-			if($this->slugConfigs['SlugConfig']['ignore_archives']) {
+			if ($this->slugConfigs['SlugConfig']['ignore_archives']) {
 				$out = preg_replace($pattern, 'href="$1' . DS . $no . '"', $out);
 			} else {
 				$out = preg_replace($pattern, 'href="$1' . '/archives/' . $no . '"', $out);
 			}
 		} else {
-			if($this->slugConfigs['SlugConfig']['ignore_archives']) {
+			if ($this->slugConfigs['SlugConfig']['ignore_archives']) {
 				$out = preg_replace($pattern, 'href="$1' . DS . '$2' . '"', $out);
 			}
 		}
-
+		
 		return $out;
-
 	}
+	
 /**
  * afterElement
  * 
- * @param string $name
- * @param string $out
+ * @param CakeEvent $event
  * @return string 
  */
-	function afterElement($name, $out) {
-
-		if(empty($this->request->params['prefix']) || ($this->request->params['prefix'] != 'admin')) {
+	public function afterElement(CakeEvent $event) {
+		$View = $event->subject();
+		if (empty($View->request->params['prefix']) || ($View->request->params['prefix'] != 'admin')) {
 			// プレビュー時に Undefined index が出るため判定
-			if(!empty($this->request->params['plugin'])) {
-				if($this->request->params['plugin'] == 'blog') {
-
-					if(preg_match('/^paginations\/.*/', $name)) {
-						if($this->slugConfigs['SlugConfig']['ignore_archives']) {
-							if($this->request->params['action'] == 'archives') {
+			if (!empty($View->request->params['plugin'])) {
+				if ($View->request->params['plugin'] == 'blog') {
+					
+					if (preg_match('/^paginations\/.*/', $event->data['name'])) {
+						if ($this->slugConfigs['SlugConfig']['ignore_archives']) {
+							if ($View->request->params['action'] == 'archives') {
 								$pattern = '/href\=\"(.+?)\/archives\/(.+?)\"/';
-								$out = preg_replace($pattern, 'href="$1' . '/$2' . '"', $out);
+								$event->data['out'] = preg_replace($pattern, 'href="$1' . '/$2' . '"', $event->data['out']);
 							}
 						}
 					}
-
+					
 				}
 			}
 		}
-
-		return $out;
-
+		return $event->data['out'];
 	}
+	
 /**
  * beforeElement：未使用：コード内コメント参照
  * 
@@ -242,22 +238,18 @@ class SlugHelperEventListener extends BcHelperEventListener {
  * @param type $subDir
  * @return array $params
  */
-	function beforeElement($name, $params, $loadHelpers, $subDir) {
-
-		if(empty($this->request->params['prefix']) || ($this->request->params['prefix'] != 'admin')) {
+	public function beforeElement($name, $params, $loadHelpers, $subDir) {
+		if (empty($this->request->params['prefix']) || ($this->request->params['prefix'] != 'admin')) {
 			// if($name == 'paginations/simple' || $name == 'paginations/default') {
-			if(preg_match('/^paginations\/.*/', $name)) {
-				if($this->request->params['action'] == 'archives') {
+			if (preg_match('/^paginations\/.*/', $name)) {
+				if ($this->request->params['action'] == 'archives') {
 					// ここで action を省略しても、最終的に Router:LINE:800 で index が付けられてしまう
 					// unset($this->View->passedArgs['action']);
 					// $this->View->passedArgs['action'] = '';
 				}
 			}
-
 		}
-
 		return $params;
-
 	}
-
+	
 }
