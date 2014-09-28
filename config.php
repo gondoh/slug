13332adc4fb3c378ab8f56e7bd1d641d0338a514
arata <contact@materializing.net>
2014-09-28 16:05:40 +0900

    fixed インストール時のメッセージを変更

--- config.php
+++ config.php
@@ -12,4 +12,4 @@ $description = 'ブログ記事のURLにスラッグを設定できます。';
 $author = 'arata';
 $url = 'http://www.materializing.net/';
 $adminLink = array('plugin' => 'slug', 'controller' => 'slug_configs', 'action' => 'index');
-$installMessage = 'ブログ記事の投稿画面にスラッグ設定欄が追加されます。<br />また、スラッグ一括設定より作成しているブログ記事のスラッグをまとめて登録できます。';
+$installMessage = 'インストール後、ブログ別のブログ基本設定画面で、スラッグ設定を行なってください。';
